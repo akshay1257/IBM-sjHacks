10ef1046d0f24c9294ca0879bed2b4d46a8779bc
akshay goel <akshaygoel1257@gmail.com>
2018-09-08 17:05:10 -0700

    Created a donor component

---
+++ client/src/app/donor.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import{Donor} from './donor';
+import 'rxjs/add/operator/map';
+
+
+@Injectable()
+export class DonorService {
+
+  constructor() { }
+
+}
---
+++ client/src/app/donor.ts
@@ -0,0 +1,8 @@
+export class Donor{
+    d_name: string;
+    addr: string;
+    items: {
+       item1: string,
+       item2: string
+    }
+}
