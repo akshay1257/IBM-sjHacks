054c43fe04b571980244c994457f0cdafe2557cb
akshay goel <akshaygoel1257@gmail.com>
2018-09-08 17:34:01 -0700

    Created service that makes call to Rest Api's

--- client/src/app/donor.service.ts
+++ client/src/app/donor.service.ts
@@ -7,6 +7,19 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class DonorService {
 
-  constructor() { }
+  constructor(private http: Http) { }
+  
+  //get requirements
+  getRequirements(){
+
+    return this.http.get('https://7647c8e2.ngrok.io/get_items').map(res=> res.json);
+  }
+  //send a request
+  addHelp(newDonor){
+    var headers =  new Headers();
+    headers.append('Content-Type' , 'application/json');
+    return this. http.post('http://localhost:3000/api/contact' , newDonor,{headers:headers})
+    .map(res => res.json());
+  }
 
 }
